ffba45af0448051f077a73164f817bdbe74e6f4e
Chonapatc <kilito2546@gmail.com>
2023-12-21 15:30:36 +0700

    bro

--- 252.cpp
+++ 252.cpp
@@ -5,31 +5,41 @@ int main()
 {
     int a;
     cin>>a;
-    int x=sqrt(a);
-    int down=0,right=0,up=0,left=0,num=1,ind=1,dif=0;
-    int arr[x][x];
-    string way="down";
-    
-    while (num<a)
+    int a2=sqrt(a);
+
+    int way=0, n=1;
+    int arr[a2][a2];
+
+    for(int i =0 ; i <a2 ;i++)
     {
-    //down
-    if(ind==2)
-    {
-        dif++;
-        ind=0;
+        for(int y =0 ; y< a2 ; y++)
+        {
+            arr[i][y]=0;
+            n++;
+        }
     }
-    if(way=="down")
+    n=1;
+    int x=0,y=0,ind=1;
+    while (n<=a)
     {
-        for(int i =0;  i< a ;i ++)
-            {
-                arr[i][dif]=num;
-                num++;
-            }
-        down++;
+
+        way%=4;
+
+        if()
+        way++;
+        
+    }
+
+    for(int i =0 ; i< a2 ;i++)
+    {
+        for(int y=0 ;y<a2;y++)
+        {
+            cout << arr[i][y] <<" " ;
+        }
+        
+        cout <<endl;
     }
     
-    
-    }
     
 
 }
