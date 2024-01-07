bb8d70be11c04ecb3d3b64085a1748b54952a0d4
Chonapatc <kilito2546@gmail.com>
2024-01-07 08:52:08 +0700

    dfdf

--- bignum.cpp
+++ bignum.cpp
@@ -4,27 +4,27 @@ using namespace std;
 
 int main()
 {
-    long double a[100];
+    double a[100];
     cin>>a[0]>>a[1];
-    long double mod=1000000007;
-
-    
+    double mod=1000000007;
+    //cout << setprecision(40 )<< pow(2,32)<<endl;
+    //cout <<setprecision(40) << a[0] << " " << a[1] ; 
     a[0]=log10l(a[0]);
-    a[2]=ceil(a[0]*a[1]);
-
+    a[2]=a[0]*a[1];
+    //cout << setprecision(40) << a[2] ; 
 
     
-    //cout << fixed << setprecision(30)<<a[2]<<endl;
-    //cout << fixed << setprecision(30)<<a2<<endl;
+//     //cout << fixed << setprecision(30)<<a[2]<<endl;
+//     //cout << fixed << setprecision(30)<<a2<<endl;
 
     a[2] = fmodl(a[2],mod);
     cout << fixed <<setprecision(0)<<a[2]+1<<endl;
-    /*;
+//     /*;
     
-    string a2[10] = {to_string(a[2])};
-    long long int a3[10]= {a[2]};
-    cout << a2[0] <<endl;
-    cout  << a3[0];*/
+//     string a2[10] = {to_string(a[2])};
+//     long long int a3[10]= {a[2]};
+//     cout << a2[0] <<endl;
+//     cout  << a3[0];*/
 
 /*ex1.
 1272551081
