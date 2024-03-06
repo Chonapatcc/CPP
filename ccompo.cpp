02abbcce7e53fd807aabf71b897bd8c72d50c17f
Chonapatcc <Chonapatcz@gmail.com>
2024-03-06 21:25:01 +0700

    breatice

---
+++ ccompo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Car
+{
+
+    
+};
+
+class Garage
+{
+private:
+    Car car1;
+    Car car2;
+};
+
+
+
+
+int main()
+{
+
+
+    cout <<"cat";
+}
