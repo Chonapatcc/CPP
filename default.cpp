e3d3ff217338ccb3acfe9850cff24bae47758f8c
Chonapatcc <Chonapatcz@gmail.com>
2024-03-10 18:17:10 +0700

    d

---
+++ default.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Shop
+{
+private:
+    int data1;
+    int data2;
+
+public:
+    //Shop(int data1= int data2=0){};
+
+    
+};
+
+class Shoppricecal :public Shop
+{
+private:
+
+
+public:
+    Shoppricecal(){};
+
+};
+
+
+int main()
+{
+
+
+}
