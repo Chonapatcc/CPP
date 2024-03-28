71e6d28f87387c68d2944d544cb4b14355497c86
Chonapatcc <Chonapatcz@gmail.com>
2024-03-28 23:13:25 +0700

    test linkedlist

--- test7.cpp
+++ test7.cpp
@@ -1,32 +1,74 @@
 #include <iostream>
-
-class Animal
+using namespace std;
+struct node
 {
-private:
-    int value;
-
-public:
-    void setValue(int value)
-    {
-        this->value = value;
-    }
-
-};
-
-
-class LinkedList
-{
-public:
-    Animal **list;
     int data;
-
-
+    struct node *next ;
+    struct node *prev;
 };
 int main()
 {
-    LinkedList lst1;
-    lst1.list;
-    Animal *head = lst1.list[0];
-    for(int i= 0 ; i<10 ; i++)
-    {}
+    struct node *head = NULL;
+    struct node *list = NULL;
+    struct node *tail = NULL;
+    for(int i =0 ; i<10 ; i++)
+    {
+        struct node *new_node = (struct node *)malloc(sizeof(struct node));
+        new_node -> data = i ; 
+        new_node -> next = NULL;
+        if(head==NULL)
+        {
+            list = new_node;
+            list->prev=NULL;
+            head = list;
+        }
+        else
+        {
+            list ->next = new_node;
+            list->next->prev = list;
+            list = list->next;
+            tail = list;
+        }
+ 
+    }
+
+    list = head;
+    cout <<"start ascending"<<endl;
+    while(list->next!=NULL)
+    {
+        cout <<list->data <<endl;
+        list = list->next;
+    }
+    cout << list->data<<endl;
+    cout <<"end ascending"<<endl;
+    list = tail;
+    cout <<"start descending"<<endl;
+    while(list->prev!=NULL)
+    {
+        cout <<list->data <<endl;
+        list = list->prev;
+    }
+    cout <<"end descending"<<endl;
+
+
+    head->prev = tail;
+    tail->next= head;
+    list =head ;
+    int count =0 ;
+    cout <<"Circular" <<endl;
+    while(list->next!=NULL)
+    {
+        if(count ==100)
+        {
+            break;
+        }
+        cout <<list->data << " ";
+        list= list->next ;
+        count++;
+    }
+
+    cout <<"end circular" <<endl;
+
+
+
 }
