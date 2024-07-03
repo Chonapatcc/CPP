d0772b41314de50aa214cf69e2b00a3865802774
Chonapatcc <chonapatcz@gmail.com>
2024-07-03 23:11:41 +0700

    2

--- test.cpp
+++ test.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main()
 {
-    int arr[10] = {1,2,3,4,5};
-    for(int x :arr)
-    {
-        cout << x <<endl; 
-    }
+    int a,b;
+    a = 2;
+    b = 3;
+    cout << (double ) (a/b);
+
+        
+
 }
