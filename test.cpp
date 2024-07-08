cb0c6d9b6166eae88a156adf64038872e72bddf9
Chonapatcc <chonapatcz@gmail.com>
2024-07-08 19:48:24 +0700

    1

--- test.cpp
+++ test.cpp
@@ -5,9 +5,14 @@ using namespace std;
 
 int main()
 {
-    string cat = ("cats");
+    int i =1 ;
+    for(i =0 ;i < 10 ; i++);
+    cout << i<<endl;
 
-    cout << cat;
+    for(int i =0 ; i<10 ; i++)
+    {
+        cout << i <<endl;
+    }
 
         
 
