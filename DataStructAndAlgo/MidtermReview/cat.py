02e53514908310092feceb73a797140a9fb8ac47
Chonapatcc <chonapatcz@gmail.com>
2024-08-18 17:55:32 +0700

    1

--- DataStructAndAlgo/MidtermReview/cat.py
+++ DataStructAndAlgo/MidtermReview/cat.py
@@ -1,10 +1,2 @@
 import random
 
-# num = random.randint(2,20)
-
-# print(num)
-# for i in range(num):
-#     x = random.randint(0,10)
-lst = "1 00000000 00000000000000000000000".split();
-for it in lst:
-    print(len(it))
