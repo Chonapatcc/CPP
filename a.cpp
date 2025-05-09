f0e958fdd7cc6ae5dc83cead888dc2ac7ec5c10f
Chonapatcc <chonapatcz@gmail.com>
2025-05-09 13:40:10 +0700

    1

--- a.cpp
+++ a.cpp
@@ -1,11 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    cout << "This is a simple C++ program." << endl;
-    cout << "g" ;
-
-
-    
-}
+#include 
