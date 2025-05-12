8405c9bdb3ad7449400f50b945797fb9d46a145e
Chonapatcc <chonapatcz@gmail.com>
2025-05-13 01:04:13 +0700

    1

--- a.cpp
+++ a.cpp
@@ -5,6 +5,8 @@ int main() {
     int a = 5;
     int b = 10;
     int c = a + b;
+
+    cout << "cat"<<endl;
     std::cout << "The sum of " << a << " and " << b << " is " << c << std::endl;
     return 0;
 }
