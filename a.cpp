e5c35dc5de2461e557e31c3883a5a70d8ab641a6
Chonapatcc <chonapatcz@gmail.com>
2025-05-31 15:16:53 +0700

    1

--- a.cpp
+++ a.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <string>
+using namespace std;
 
 int main() {
-    int a = 5;
-    int b = 10;
-    int c = a + b;
+    cout << "Hello, World!" << endl;
+    cout << "This is a simple C++ program." << endl;
+    cout << "g" ;
 
-    cout << "cat"<<endl;
-    std::cout << "The sum of " << a << " and " << b << " is " << c << std::endl;
-    return 0;
+
+    
 }
